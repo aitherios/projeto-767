29de4607c03a5b165fe5e18e8df8730b984d080c
Renan Mendes Carvalho <aitherios@gmail.com>
2012-03-17 12:28:53 -0300

    bugfix: disqus was not beeing rendered when ajax was used

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,9 +4,11 @@ class HomeController < ApplicationController
 
   def index
     @content_block_name = params[:content] || 'main-page'
+    content_block = ContentBlock.find_by_name @content_block_name
+    content_block.html = render_to_string :index, layout: false
     respond_to do |format|
       format.html { render :index }
-      format.json { render json: ContentBlock.find_by_name(@content_block_name) }
+      format.json { render json: content_block }
     end
   end
 
